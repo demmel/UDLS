7d18e7ea8de2552f88c872725ce046490bc319dd
David Emmel <dgemmel2@gmail.com>
2022-12-26 21:22:54 -0800

    Allow specifying suffix for label dataset instead of only using input

--- udls/simple_dataset.py
+++ udls/simple_dataset.py
@@ -1,14 +1,10 @@
-from concurrent.futures import ProcessPoolExecutor, TimeoutError
-from os import makedirs, path
-from pathlib import Path
-
-import librosa as li
-import numpy as np
 import torch
-from scipy.io.wavfile import read as read_wav_file
+from udls import SimpleLMDBDataset
+from pathlib import Path
+import librosa as li
+from os import makedirs, path
 from tqdm import tqdm
-
-from . import SimpleLMDBDataset
+import numpy as np
 
 
 def dummy_load(name):
@@ -36,7 +32,6 @@ def simple_audio_preprocess(sampling_rate, N):
             print(e)
             return None
 
-
         pad = (N - (len(x) % N)) % N
         x = np.pad(x, (0, pad))
 
@@ -59,6 +54,7 @@ class SimpleDataset(torch.utils.data.Dataset):
         split_percent=.2,
         split_set="train",
         seed=0,
+        label_suffix=None,
     ):
         super().__init__()
 
@@ -69,13 +65,14 @@ class SimpleDataset(torch.utils.data.Dataset):
 
         self.folder_list = folder_list
         self.file_list = file_list
+        self.label_suffix = label_suffix
 
         self.preprocess_function = preprocess_function
         self.extension = extension
 
         self.transforms = transforms
 
-        # IF NO DATA INSIDE DATASET: PREPROCESS
+        # IF NO DATA INSIDE DATASET: PREPROCESS
         self.len = len(self.env)
 
         if self.len == 0:
@@ -105,31 +102,58 @@ class SimpleDataset(torch.utils.data.Dataset):
         idx = 0
         wavs = []
 
+        def input_name_with_label(file):
+            path = Path(file)
+            if self.label_suffix is not None:
+                return (path, path.with_name(f'{path.name}{self.label_suffix}'))
+            else:
+                return (path, path)
+
+        def input_names_with_labels(file_list):
+            return [input_name_with_label(file) for file in file_list]
+
         # POPULATE WAV LIST
         if self.folder_list is not None:
-            for f, folder in enumerate(self.folder_list.split(",")):
+            for folder in self.folder_list.split(","):
                 print("Recursive search in {}".format(folder))
                 for ext in extension:
-                    wavs.extend(list(Path(folder).rglob(ext)))
+                    files = list(Path(folder).rglob(ext))
+                    files = filter(
+                        lambda file: not file.name.endswith(self.label_suffix),
+                        files
+                    ) if self.label_suffix is not None else files
+                    wavs.extend(
+                        input_names_with_labels(files)
+                    )
 
         else:
             with open(self.file_list, "r") as file_list:
-                wavs = file_list.read().split("\n")
+                wavs = input_names_with_labels(file_list.read().split("\n"))
 
         loader = tqdm(wavs)
-        for wav in loader:
-            loader.set_description("{}".format(path.basename(wav)))
-            output = self.preprocess_function(wav)
-            if output is not None:
-                for o in output:
-                    self.env[idx] = o
-                    idx += 1
+        for (input, label) in loader:
+            loader.set_description("{}".format(path.basename(input)))
+            input_preprocessed = self.preprocess_function(input)
+            label_preprocessed = input_preprocessed if input == label else self.preprocess_function(
+                label)
+
+            if input_preprocessed is None or label_preprocessed is None:
+                print(f"No data for {input}")
+            elif len(input_preprocessed) != len(label_preprocessed):
+                print(
+                    f"Input length '{len(input_preprocessed)}' does not match label length '{len(label_preprocessed)}' for {input}"
+                )
+
+            for o in zip(input_preprocessed, label_preprocessed):
+                self.env[idx] = o
+                idx += 1
 
     def __len__(self):
         return self.len
 
     def __getitem__(self, index):
-        data = self.env[self.index[index + self.offset]]
+        (input, label) = self.env[self.index[index + self.offset]]
         if self.transforms is not None:
-            data = self.transforms(data)
-        return data
+            input = self.transforms(input)
+            label = self.transforms(label)
+        return (input, label)
